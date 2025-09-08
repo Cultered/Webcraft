3ea7a43be06205484279cf1a8d2a352454143c5d
AlexBelov <mail.alexbelov@gmail.com>
2025-09-08 15:38:56 +0400

    some thoughts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 
 import type { Optimizations } from '../Types/Optimizations';
 export const o11s: Optimizations = {
-    CPU_CHUNKS: true,
+    CPU_CHUNKS: true,//will probably be moved to gpu
     CHUNK_SIZE: 300,
     RENDER_DISTANCE: 5,
     LOD_DISTANCE: 1,
-    CPU_SOFT_FRUSTUM_CULLING: false,//needs fixing
-    CPU_LOD: false,//needs mega fixing
+    CPU_SOFT_FRUSTUM_CULLING: false,//will probably be deprecated too
+    CPU_LOD: false,//will be deprecated
     USE_WEBGPU:true//WebGL lighting implementation also working
 }
